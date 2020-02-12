4b3d701ee3c87cb1dd0db13e06442862faa660bf
Joonas Partanen <partanen.joonas@gmail.com>
2020-02-12 22:08:27 +0200

    Rename dataUtils to dataService

---
+++ fft_backend/utils/SITC2Array.js
@@ -0,0 +1,20 @@
+const SITC2Array = [
+  { group: 'Food and live animals', children: [] },
+  { group: 'Beverages and tobacco', children: [] },
+  { group: 'Crude materials, inedible, except fuels', children: [] },
+  { group: 'Mineral fuels, lubricants and related materials', children: [] },
+  { group: 'Animal and vegetable oils, fats and waxes', children: [] },
+  { group: 'Chemicals and related products', children: [] },
+  {
+    group: 'Manufactured goods classified chiefly by material',
+    children: []
+  },
+  { group: 'Machinery and transport equipment', children: [] },
+  { group: 'Miscellaneous manufactured articles', children: [] },
+  {
+    group: 'Commodities and transactions not classified elsewhere',
+    children: []
+  }
+]
+
+module.exports = SITC2Array
