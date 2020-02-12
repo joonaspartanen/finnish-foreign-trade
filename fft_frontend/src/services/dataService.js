d3ad6c548296b721b8b5596534a2fae5d3a3ef8c
Joonas Partanen <partanen.joonas@gmail.com>
2020-02-12 22:09:07 +0200

    Add helper function for fetching SITC2 data

---
+++ fft_frontend/src/services/dataService.js
@@ -0,0 +1,34 @@
+import axios from 'axios'
+
+const getImports = async () => {
+  const response = await axios.get('/imports')
+  return response
+}
+
+const getExports = async () => {
+  const response = await axios.get('/exports')
+  return response
+}
+
+const getTradeBalance = async () => {
+  const response = await axios.get('/tradebalance')
+  return response
+}
+
+const getProductData = async () => {
+  const response = await axios.get('/SITC1')
+  return response
+}
+
+const getSITC2Data = async flow => {
+  const response = await axios.get(`/SITC2/${flow}`)
+  return response
+}
+
+export default {
+  getImports,
+  getExports,
+  getTradeBalance,
+  getProductData,
+  getSITC2Data
+}
