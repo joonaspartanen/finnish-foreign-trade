ce894d4a893ca65957ff7677c6574a3ea8551c1e
Joonas Partanen <partanen.joonas@gmail.com>
2020-02-13 19:20:07 +0200

    Install and implement memory-cache

---
+++ fft_backend/utils/middleware.js
@@ -0,0 +1,23 @@
+const memorycache = require('memory-cache')
+
+const cache = duration => {
+  return (req, res, next) => {
+    const key = '__express__' + req.originalUrl || req.originalUrl
+    const cacheContent = memorycache.get(key)
+    if (cacheContent) {
+      console.log('fetching from cache')
+      res.send(cacheContent)
+      return
+    } else {
+      console.log('fetching from uljas api')
+      res.sendResponse = res.send
+      res.send = body => {
+        memorycache.put(key, body, duration * 1000)
+        res.sendResponse(body)
+      }
+      next()
+    }
+  }
+}
+
+module.exports = { cache: cache }
