c2bf33dc3a1d5761e2a996917fe9ce68ec7b83ee
Joonas Partanen <partanen.joonas@gmail.com>
2020-02-19 22:52:02 +0200

    Create service functions to fetch country-specific SITC2 data from API

--- fft_frontend/src/services/dataService.js
+++ fft_frontend/src/services/dataService.js
@@ -20,9 +20,15 @@ const getSITC2Data = async flow => {
   return response
 }
 
+const getSITC2CountryData = async (flow, country) => {
+  const response = await axios.get(`/SITC2/${flow}/${country}`)
+  return response
+}
+
 export default {
   getImports,
   getExports,
   getTradeBalance,
-  getSITC2Data
+  getSITC2Data,
+  getSITC2CountryData
 }
