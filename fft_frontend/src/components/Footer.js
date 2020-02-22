f9930ad4511a075a8065882a127d8147db4b9561
Joonas Partanen <partanen.joonas@gmail.com>
2020-02-22 22:23:10 +0200

    Add new Footer component

---
+++ fft_frontend/src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Container, Menu, Icon } from 'semantic-ui-react'
+
+const Footer = () => {
+  return (
+    <Menu inverted secondary style={{ backgroundColor: '#333', marginTop: '0px' }}>
+      <Container fluid>
+        <Menu.Item link as='a' href='https://github.com/joonaspartanen/finnish-foreign-trade'>
+          About
+        </Menu.Item>
+      </Container>
+    </Menu>
+  )
+}
+
+export default Footer
