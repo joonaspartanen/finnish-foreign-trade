65f6013d485c0ebcfc330a4a9e8d0fb3126c01bd
Joonas Partanen <partanen.joonas@gmail.com>
2020-02-22 22:23:26 +0200

    Add new NavBar component

---
+++ fft_frontend/src/components/NavBar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Container, Menu, Icon } from 'semantic-ui-react'
+
+const NavBar = () => {
+  return (
+    <Menu inverted size='massive' style={{ backgroundColor: '#333', marginBottom: '0px' }}>
+      <Container fluid>
+        <Menu.Item header>
+          <Icon name='euro sign' size='large'></Icon>
+          Finnish Foreign Trade Visualized</Menu.Item>
+          <Menu.Item link as='a' href='#trade-partners'>
+          Trade Partners
+        </Menu.Item>
+        <Menu.Item link as='a' href='#trade-balance'>
+          Trade Balance
+        </Menu.Item>
+        <Menu.Item link as='a' href='#imports-by-product'>
+          By product group
+        </Menu.Item>
+      </Container>
+    </Menu>
+  )
+}
+
+export default NavBar
