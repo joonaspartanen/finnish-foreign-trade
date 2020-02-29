85d70de148d4bf8dec666de9ff6df9f92e0b025b
Joonas Partanen <partanen.joonas@gmail.com>
2020-02-29 14:10:42 +0200

    Submit semantic-ui for bootstrap

--- fft_frontend/src/components/CountrySearch.js
+++ fft_frontend/src/components/CountrySearch.js
@@ -1,21 +1,17 @@
 import React from 'react'
 import { Form } from 'react-bootstrap'
+import { Input } from 'semantic-ui-react'
 
 const CountrySearch = ({ countryFilter, handleCountryFilterChange }) => {
   return (
-    <Form>
-      <Form.Group controlId='countryName'>
-        <h2 style={{ textAlign: 'center' }}>Search country</h2>
-        <Form.Control
-          type='text'
-          value={countryFilter}
-          onChange={event => {
-            console.log(event.target.value)
-            handleCountryFilterChange(event.target.value)
-          }}
-        />
-      </Form.Group>
-    </Form>
+    <Input
+      icon='search'
+      placeholder='Search...'
+      value={countryFilter}
+      onChange={event => {
+        console.log(event.target.value)
+        handleCountryFilterChange(event.target.value)
+      }}></Input>
   )
 }
 
