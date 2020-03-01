6fdb9aaf818f6626a06d373b878912b316b97892
Joonas Partanen <partanen.joonas@gmail.com>
2020-03-01 18:02:57 +0200

    Substitute proper semantic-ui Search component with dropdown list for old search input

--- fft_frontend/src/components/CountrySearch.js
+++ fft_frontend/src/components/CountrySearch.js
@@ -1,17 +1,62 @@
-import React from 'react'
-import { Form } from 'react-bootstrap'
-import { Input } from 'semantic-ui-react'
+import React, { useState } from 'react'
+import { Search } from 'semantic-ui-react'
+import _ from 'lodash'
 
-const CountrySearch = ({ countryFilter, handleCountryFilterChange }) => {
+const CountrySearch = ({ handleCountryFilterChange, countryNames }) => {
+  const [value, setValue] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
+  const [results, setResults] = useState([])
+
+  const resultRenderer = ({ title }) => <div>{title}</div>
+
+  const handleResultSelect = (e, { result }) => {
+    setValue(result.title)
+    handleCountryFilterChange(result.title)
+  }
+
+  const handleSearchChange = (e, { value }) => {
+    setIsLoading(true)
+    setValue(value)
+
+    setIsLoading(true)
+    setValue(value)
+
+    if (value.length < 1) {
+      setValue('')
+      setIsLoading(false)
+      setResults([])
+      return
+    }
+
+    const re = new RegExp(_.escapeRegExp(value), 'i')
+    const isMatch = result => re.test(result.title)
+
+    setIsLoading(false)
+
+    const foundResults = _.filter(countryNames, isMatch)
+
+    if (foundResults.length > 20) {
+      setResults([{ title: 'Please narrow down your search.' }])
+    } else {
+      setResults(foundResults)
+    }
+    if (results.length === 1) {
+      handleCountryFilterChange(results[0].title)
+    }
+    console.log(results)
+  }
+
   return (
-    <Input
-      icon='search'
+    <Search
       placeholder='Search...'
-      value={countryFilter}
-      onChange={event => {
-        console.log(event.target.value)
-        handleCountryFilterChange(event.target.value)
-      }}></Input>
+      loading={isLoading}
+      onResultSelect={handleResultSelect}
+      onSearchChange={_.debounce(handleSearchChange, 500, {
+        leading: true
+      })}
+      results={results}
+      resultRenderer={resultRenderer}
+      value={value}></Search>
   )
 }
 
