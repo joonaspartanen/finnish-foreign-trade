33f5b455e5a80353bde9ee25ddd9bc53b77b2a8f
Joonas Partanen <partanen.joonas@gmail.com>
2020-03-01 11:31:22 +0200

    Rename component to render flow selection buttons and substitute semantic-ui for bootstrap

---
+++ fft_frontend/src/components/FlowButtons.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import { Button } from 'semantic-ui-react'
+
+const FlowButtons = ({ setFlow }) => {
+  const [exportsActive, setExportsActive] = useState(true)
+
+  return (
+    <div style={{ position: 'absolute', bottom: '2em', left: '2em' }}>
+      <Button.Group>
+        <Button
+          toggle
+          active={exportsActive}
+          content='Exports'
+          onClick={value => {
+            if (!exportsActive) {
+              setExportsActive(!exportsActive)
+              setFlow('exports')
+            }
+          }}
+        />
+        <Button
+          toggle
+          active={!exportsActive}
+          content='Imports'
+          onClick={value => {
+            if (exportsActive) {
+              setExportsActive(!exportsActive)
+              setFlow('imports')
+            }
+          }}
+        />
+      </Button.Group>
+    </div>
+  )
+}
+
+export default FlowButtons
