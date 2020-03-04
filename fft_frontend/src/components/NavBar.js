28700dab8e10b8715ea0bf83e3da056c33f20ba2
Joonas Partanen <partanen.joonas@gmail.com>
2020-03-04 18:07:20 +0200

    Remove unnecessary parent container that broke the mobile view

--- fft_frontend/src/components/NavBar.js
+++ fft_frontend/src/components/NavBar.js
@@ -3,24 +3,20 @@ import { Container, Menu, Icon } from 'semantic-ui-react'
 
 const NavBar = () => {
   return (
-    <Menu inverted size='massive' style={{ backgroundColor: '#333', marginBottom: '0px' }}>
-      <Container fluid>
-        <Menu.Item header>
-          <Icon name='euro sign' size='large'></Icon>
-          Finnish Foreign Trade Visualized</Menu.Item>
-          <Menu.Item link as='a' href='#trade-map'>
-          Trade Map
-        </Menu.Item>
-        <Menu.Item link as='a' href='#trade-balance'>
-          Trade Balance
-        </Menu.Item>
-        <Menu.Item link as='a' href='#imports-by-product'>
-          By product group
-        </Menu.Item>
-        <Menu.Item link as='a' href='#trade-partners'>
-          By trade partner
-        </Menu.Item>
-      </Container>
+    <Menu inverted stackable size='large' style={{ backgroundColor: '#333', marginBottom: '0px' }}>
+      <Menu.Item header>Finnish Foreign Trade Visualized</Menu.Item>
+      <Menu.Item link as='a' href='#trade-map'>
+        Trade Map
+      </Menu.Item>
+      <Menu.Item link as='a' href='#trade-balance'>
+        Trade Balance
+      </Menu.Item>
+      <Menu.Item link as='a' href='#imports-by-product'>
+        By product group
+      </Menu.Item>
+      <Menu.Item link as='a' href='#trade-partners'>
+        By trade partner
+      </Menu.Item>
     </Menu>
   )
 }
