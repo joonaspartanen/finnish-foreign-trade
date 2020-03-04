85d0309e0b7f2bfae43cee55c453f3d4818710b5
Joonas Partanen <partanen.joonas@gmail.com>
2020-03-04 21:52:32 +0200

    Remove repetitive code
    This was a preliminary solution to rotate the chart on mobile, but a better way must be found.

--- fft_frontend/src/components/TradeBalanceChart.js
+++ fft_frontend/src/components/TradeBalanceChart.js
@@ -11,115 +11,86 @@ const TradeBalanceChart = ({ tradeBalance }) => {
     am4core.useTheme(am4themes_spiritedaway)
 
     let chart = am4core.create('chartdiv', am4charts.XYChart)
+    chart.responsive.enabled = true
+    chart.rotate = true
+
     chart.data = tradeBalance
     chart.marginRight = '100'
 
-    if (window.innerHeight > window.innerWidth) {
-      let categoryAxis = chart.yAxes.push(new am4charts.CategoryAxis())
-      categoryAxis.dataFields.category = 'year'
-      categoryAxis.numberFormatter.numberFormat = '#'
-      categoryAxis.renderer.inversed = true
-      categoryAxis.renderer.grid.template.location = 0
-      categoryAxis.renderer.cellStartLocation = 0.1
-      categoryAxis.renderer.cellEndLocation = 0.9
+    let categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis())
+    categoryAxis.dataFields.category = 'year'
+    categoryAxis.numberFormatter.numberFormat = '#'
+    categoryAxis.renderer.inversed = true
+    categoryAxis.renderer.grid.template.location = 0
+    categoryAxis.renderer.cellStartLocation = 0.1
+    categoryAxis.renderer.cellEndLocation = 0.9
 
-      let valueAxis = chart.xAxes.push(new am4charts.ValueAxis())
-      valueAxis.min = -1000000000
-      valueAxis.strictMinMax = true
-      valueAxis.numberFormatter.numberFormat = '##.##a'
-      valueAxis.numberFormatter.bigNumberPrefixes = [{ number: 1e9, suffix: 'B' }]
+    let valueAxis = chart.yAxes.push(new am4charts.ValueAxis())
+    // valueAxis.min = 0
+    valueAxis.numberFormatter.numberFormat = '##.##a'
+    valueAxis.numberFormatter.bigNumberPrefixes = [{ number: 1e9, suffix: 'B' }]
 
-      valueAxis.renderer.opposite = true
+    let tradeBalanceSeries = chart.series.push(new am4charts.ColumnSeries())
+    tradeBalanceSeries.name = 'Trade Balance'
+    tradeBalanceSeries.fill = am4core.color('#2E8B57')
+    tradeBalanceSeries.strokeWidth = 0
+    tradeBalanceSeries.dataFields.valueY = 'tradeBalance'
+    tradeBalanceSeries.dataFields.categoryX = 'year'
+    tradeBalanceSeries.columns.template.height = am4core.percent(100)
+    tradeBalanceSeries.sequencedInterpolation = true
+    tradeBalanceSeries.columns.template.tooltipText = '{name}:\n€{valueY}'
 
-      let tradeBalanceSeries = chart.series.push(new am4charts.ColumnSeries())
-      tradeBalanceSeries.name = 'Trade Balance'
-      tradeBalanceSeries.fill = am4core.color('#2E8B57')
-      tradeBalanceSeries.strokeWidth = 0
-      tradeBalanceSeries.dataFields.valueX = 'tradeBalance'
-      tradeBalanceSeries.dataFields.categoryY = 'year'
-      tradeBalanceSeries.columns.template.height = am4core.percent(100)
-      tradeBalanceSeries.sequencedInterpolation = true
-      tradeBalanceSeries.columns.template.tooltipText = '{name}:\n€{valueX}'
+    tradeBalanceSeries.columns.template.adapter.add('fill', (fill, target) => {
+      if (target.dataItem && target.dataItem.valueY < 0) {
+        return am4core.color('#800000')
+      } else {
+        return fill
+      }
+    })
 
-      tradeBalanceSeries.columns.template.adapter.add('fill', (fill, target) => {
-        if (target.dataItem && target.dataItem.valueX < 0) {
-          return am4core.color('#800000')
-        } else {
-          return fill
+    let importSeries = chart.series.push(new am4charts.ColumnSeries())
+    importSeries.name = 'Imports to Finland'
+    importSeries.fill = am4core.color('#63718B')
+    importSeries.strokeWidth = 0
+    importSeries.dataFields.valueY = 'imports'
+    importSeries.dataFields.categoryX = 'year'
+    importSeries.columns.template.height = am4core.percent(100)
+    importSeries.sequencedInterpolation = true
+    importSeries.columns.template.tooltipText = '{name}:\n€{valueY}'
+
+    let exportSeries = chart.series.push(new am4charts.ColumnSeries())
+    exportSeries.name = 'Exports from Finland'
+    exportSeries.fill = am4core.color('#EBA05C')
+    exportSeries.strokeWidth = 0
+    exportSeries.dataFields.valueY = 'exports'
+    exportSeries.dataFields.categoryX = 'year'
+    exportSeries.columns.template.height = am4core.percent(100)
+    exportSeries.sequencedInterpolation = true
+    exportSeries.columns.template.tooltipText = '{name}:\n€{valueY}'
+
+    /* chart.responsive.rules.push({
+      relevant: am4core.ResponsiveBreakpoints.widthXL,
+      state: (target, stateId) => {
+        if (target instanceof am4charts.Chart) {
+          let state = target.states.create(stateId)
+          console.log(state)
+          return state
         }
-      })
 
-      let importSeries = chart.series.push(new am4charts.ColumnSeries())
-      importSeries.name = 'Imports to Finland (2018)'
-      importSeries.fill = am4core.color('#63718B')
-      importSeries.strokeWidth = 0
-      importSeries.dataFields.valueX = 'imports'
-      importSeries.dataFields.categoryY = 'year'
-      importSeries.columns.template.height = am4core.percent(100)
-      importSeries.sequencedInterpolation = true
-      importSeries.columns.template.tooltipText = '{name}:\n€{valueX}'
-
-      let exportSeries = chart.series.push(new am4charts.ColumnSeries())
-      exportSeries.name = 'Exports from Finland (2018)'
-      exportSeries.fill = am4core.color('#EBA05C')
-      exportSeries.strokeWidth = 0
-      exportSeries.dataFields.valueX = 'exports'
-      exportSeries.dataFields.categoryY = 'year'
-      exportSeries.columns.template.height = am4core.percent(100)
-      exportSeries.sequencedInterpolation = true
-      exportSeries.columns.template.tooltipText = '{name}:\n€{valueX}'
-    } else {
-      let categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis())
-      categoryAxis.dataFields.category = 'year'
-      categoryAxis.numberFormatter.numberFormat = '#'
-      categoryAxis.renderer.inversed = true
-      categoryAxis.renderer.grid.template.location = 0
-      categoryAxis.renderer.cellStartLocation = 0.1
-      categoryAxis.renderer.cellEndLocation = 0.9
-
-      let valueAxis = chart.yAxes.push(new am4charts.ValueAxis())
-      // valueAxis.min = 0
-      valueAxis.numberFormatter.numberFormat = '##.##a'
-      valueAxis.numberFormatter.bigNumberPrefixes = [{ number: 1e9, suffix: 'B' }]
-
-      let tradeBalanceSeries = chart.series.push(new am4charts.ColumnSeries())
-      tradeBalanceSeries.name = 'Trade Balance'
-      tradeBalanceSeries.fill = am4core.color('#2E8B57')
-      tradeBalanceSeries.strokeWidth = 0
-      tradeBalanceSeries.dataFields.valueY = 'tradeBalance'
-      tradeBalanceSeries.dataFields.categoryX = 'year'
-      tradeBalanceSeries.columns.template.height = am4core.percent(100)
-      tradeBalanceSeries.sequencedInterpolation = true
-      tradeBalanceSeries.columns.template.tooltipText = '{name}:\n€{valueY}'
-
-      tradeBalanceSeries.columns.template.adapter.add('fill', (fill, target) => {
-        if (target.dataItem && target.dataItem.valueY < 0) {
-          return am4core.color('#800000')
-        } else {
-          return fill
+        if (target instanceof am4charts.CategoryAxis) {
+          let state = target.states.create(stateId)
+          console.log(state.sprite.renderer)
+          return state
         }
-      })
-
-      let importSeries = chart.series.push(new am4charts.ColumnSeries())
-      importSeries.name = 'Imports to Finland'
-      importSeries.fill = am4core.color('#63718B')
-      importSeries.strokeWidth = 0
-      importSeries.dataFields.valueY = 'imports'
-      importSeries.dataFields.categoryX = 'year'
-      importSeries.columns.template.height = am4core.percent(100)
-      importSeries.sequencedInterpolation = true
-      importSeries.columns.template.tooltipText = '{name}:\n€{valueY}'
-
-      let exportSeries = chart.series.push(new am4charts.ColumnSeries())
-      exportSeries.name = 'Exports from Finland'
-      exportSeries.fill = am4core.color('#EBA05C')
-      exportSeries.strokeWidth = 0
-      exportSeries.dataFields.valueY = 'exports'
-      exportSeries.dataFields.categoryX = 'year'
-      exportSeries.columns.template.height = am4core.percent(100)
-      exportSeries.sequencedInterpolation = true
-      exportSeries.columns.template.tooltipText = '{name}:\n€{valueY}'
-    }
+        if (target instanceof am4charts.ValueAxis) {
+          let state = target.states.create(stateId)
+          console.log(state)
+          //state.sprite.renderer.opposite = true
+          return state
+        }
+        return null
+      }
+    }) */
 
     return () => {
       if (chart) {
