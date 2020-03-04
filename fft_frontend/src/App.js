96c018fc115ec7eaa2b0684e6eadc982862f8df4
Joonas Partanen <partanen.joonas@gmail.com>
2020-03-04 18:11:12 +0200

    Reduce padding to allow more space for chart

--- fft_frontend/src/App.js
+++ fft_frontend/src/App.js
@@ -104,7 +104,7 @@ const App = () => {
                   height: '100vh',
                   backgroundColor: '#222',
                   position: 'relative',
-                  padding: '0 0 6em 0'
+                  padding: '0 0 3em 0'
                 }}>
                 <ProductsTreeMap SITC2Data={importsSITC2} flow={'imports'} />
                 <a href='#exports-by-product' style={{ position: 'absolute', bottom: '2em' }}>
