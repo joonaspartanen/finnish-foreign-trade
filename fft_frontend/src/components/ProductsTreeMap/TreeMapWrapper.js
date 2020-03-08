b2cec511081766e8226106505c61f989d148a81e
Joonas Partanen <partanen.joonas@gmail.com>
2020-03-08 20:42:59 +0200

    Render Dimmer if data is not ready

--- fft_frontend/src/components/ProductsTreeMap/TreeMapWrapper.js
+++ fft_frontend/src/components/ProductsTreeMap/TreeMapWrapper.js
@@ -1,15 +1,23 @@
 import React, { useState } from 'react'
 import ProductsTreeMap from './ProductsTreeMap'
 import Controls from './Controls'
-import { Container, Grid } from 'semantic-ui-react'
+import { Container, Grid, Dimmer, Loader } from 'semantic-ui-react'
 
 const TreeMapWrapper = ({ SITC2Data }) => {
   const [flow, setFlow] = useState('exports')
 
+  if (SITC2Data === undefined) {
+    return (
+      <Dimmer active>
+        <Loader />
+      </Dimmer>
+    )
+  }
+
   return (
     <Container fluid>
       <Grid>
-        <Grid.Column width={4} verticalAlign='middle' style={{paddingLeft: '10em'}}>
+        <Grid.Column width={4} verticalAlign='middle' style={{ paddingLeft: '10em' }}>
           <Controls flow={flow} setFlow={setFlow} />
         </Grid.Column>
         <Grid.Column width={12}>
