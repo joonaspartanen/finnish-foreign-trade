981acc414705de0957e4619b871242544219e060
Joonas Partanen <partanen.joonas@gmail.com>
2020-03-08 10:54:23 +0200

    Substitute Controls for FlowButtons to control treemap

--- fft_frontend/src/components/ProductsTreeMap/TreeMapWrapper.js
+++ fft_frontend/src/components/ProductsTreeMap/TreeMapWrapper.js
@@ -1,15 +1,22 @@
 import React, { useState } from 'react'
 import ProductsTreeMap from './ProductsTreeMap'
-import FlowButtons from '../FlowButtons/FlowButtons'
+import Controls from './Controls'
+import { Container, Grid } from 'semantic-ui-react'
 
 const TreeMapWrapper = ({ SITC2Data }) => {
   const [flow, setFlow] = useState('exports')
 
   return (
-    <>
-      <FlowButtons setFlow={setFlow} />
-      <ProductsTreeMap SITC2Data={SITC2Data} flow={flow} />
-    </>
+    <Container fluid>
+      <Grid>
+        <Grid.Column width={4} verticalAlign='middle' style={{paddingLeft: '10em'}}>
+          <Controls flow={flow} setFlow={setFlow} />
+        </Grid.Column>
+        <Grid.Column width={12}>
+          <ProductsTreeMap SITC2Data={SITC2Data} flow={flow} />
+        </Grid.Column>
+      </Grid>
+    </Container>
   )
 }
 
