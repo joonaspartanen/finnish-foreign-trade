526681b0b9dde22b2c99bccc8677838ae0c6b1ca
Joonas Partanen <partanen.joonas@gmail.com>
2020-03-08 10:52:49 +0200

    Remove unnecessary Container

--- fft_frontend/src/components/Footer/Footer.js
+++ fft_frontend/src/components/Footer/Footer.js
@@ -1,14 +1,12 @@
 import React from 'react'
-import { Container, Menu } from 'semantic-ui-react'
+import { Menu } from 'semantic-ui-react'
 
 const Footer = () => {
   return (
     <Menu inverted secondary style={{ backgroundColor: '#333', marginTop: '0px' }}>
-      <Container fluid>
-        <Menu.Item link as='a' href='https://github.com/joonaspartanen/finnish-foreign-trade'>
-          About
-        </Menu.Item>
-      </Container>
+      <Menu.Item link as='a' href='https://github.com/joonaspartanen/finnish-foreign-trade'>
+        About
+      </Menu.Item>
     </Menu>
   )
 }
