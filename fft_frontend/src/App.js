79170f06816129d1e93789f8ec880cfced2557e8
Joonas Partanen <partanen.joonas@gmail.com>
2020-07-09 18:31:41 +0300

    Wrap map and flow buttons in wrapper component

--- fft_frontend/src/App.js
+++ fft_frontend/src/App.js
@@ -2,17 +2,15 @@ import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import ScrollableAnchor from 'react-scrollable-anchor'
 import { Container, Loader } from 'semantic-ui-react'
-import './App.css'
+import './App.scss'
 import CountryDataWrapper from './components/CountryData/CountryDataWrapper'
 import CountrySearch from './components/CountryData/CountrySearch'
-import FlowButtons from './components/FlowButtons/FlowButtons'
 import Footer from './components/Footer/Footer'
-import Map from './components/Map/Map'
+import MapWrapper from './components/Map/MapWrapper'
 import NavBar from './components/NavBar/NavBar'
 import TreeMapWrapper from './components/ProductsTreeMap/TreeMapWrapper'
 import TradeBalanceChart from './components/TradeBalanceChart/TradeBalanceChart'
 import { initializeTradeData } from './reducers/tradeDataReducer'
-import getCountryCodes from './services/countryService'
 import { initializeCountryCodes } from './reducers/countryReducer'
 
 const App = () => {
@@ -22,13 +20,13 @@ const App = () => {
   const [country, setCountry] = useState([])
   const [countryFilter, setCountryFilter] = useState('')
 
-  const handleCountryFilterChange = (countryName) => {
+  const handleCountryFilterChange = countryName => {
     setCountryFilter(countryName)
-    const country = countryCodes.filter((c) => c.name === countryName)
+    const country = countryCodes.filter(c => c.name === countryName)
     setCountry(country)
   }
 
-  const state = useSelector((state) => state)
+  const state = useSelector(state => state)
   const tradeData = state.tradeData
   const countryCodes = state.countryData.countryCodes
   console.log(state)
@@ -50,59 +48,31 @@ const App = () => {
         {tradeData.importsData !== undefined && tradeData.exportsData !== undefined && (
           <div>
             <ScrollableAnchor id={'trade-map'}>
-              <div
-                className='section'
-                style={{
-                  position: 'relative',
-                  height: 'calc(100vh - 60px)',
-                  backgroundColor: '#333',
-                }}>
-                <Map tradeData={tradeData} />
-                <FlowButtons />
+              <section className='chart-section first'>
+                <MapWrapper imports={tradeData.importsData} exports={tradeData.exportsData}></MapWrapper>
                 <a href='#trade-balance' style={{ position: 'absolute', bottom: '2em' }}>
                   <div className='arrow-down'></div>
                 </a>
-              </div>
+              </section>
             </ScrollableAnchor>
             <ScrollableAnchor id={'trade-balance'}>
-              <div
-                className='section'
-                style={{
-                  height: '100vh',
-                  backgroundColor: '#222',
-                  position: 'relative',
-                  padding: '0 0 4em 0',
-                }}>
+              <section className='chart-section'>
                 <TradeBalanceChart tradeBalance={tradeData.tradeBalance} />
                 <a href='#imports-by-product' style={{ position: 'absolute', bottom: '2em' }}>
                   <div className='arrow-down'></div>
                 </a>
-              </div>
+              </section>
             </ScrollableAnchor>
             <ScrollableAnchor id={'imports-by-product'}>
-              <div
-                className='section'
-                style={{
-                  height: '100vh',
-                  backgroundColor: '#333',
-                  position: 'relative',
-                  padding: '0 0 3em 0',
-                }}>
+              <section className='chart-section'>
                 <TreeMapWrapper sitc2Data={tradeData.sitc2Data} />
                 <a href='#trade-partners' style={{ position: 'absolute', bottom: '2em' }}>
                   <div className='arrow-down'></div>
                 </a>
-              </div>
+              </section>
             </ScrollableAnchor>
             <ScrollableAnchor id={'trade-partners'}>
-              <div
-                className='section'
-                style={{
-                  minHeight: 'calc(100vh - 40px)',
-                  backgroundColor: '#222',
-                  position: 'relative',
-                  padding: '0 0 3em 0',
-                }}>
+              <section className='chart-section'>
                 {country.length === 0 && (
                   <CountrySearch
                     countryFilter={countryFilter}
@@ -118,7 +88,7 @@ const App = () => {
                     year={year}
                   />
                 )}
-              </div>
+              </section>
             </ScrollableAnchor>
           </div>
         )}
