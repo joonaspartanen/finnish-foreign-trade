f25e66d087c7e71dbd8320e49e3df3f8bce52523
Joonas Partanen <partanen.joonas@gmail.com>
2020-07-10 23:15:20 +0300

    Implement preliminary dark/light mode styling

--- fft_frontend/src/components/Footer/Footer.js
+++ fft_frontend/src/components/Footer/Footer.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Menu } from 'semantic-ui-react'
 
-const Footer = () => {
+const Footer = ({ darkModeActive }) => {
   return (
-    <Menu inverted secondary style={{ backgroundColor: '#333', marginTop: '0px' }}>
+    <Menu secondary className={darkModeActive ? 'footer inverted dark-mode' : 'footer'} >
       <Menu.Item link as='a' href='https://github.com/joonaspartanen/finnish-foreign-trade'>
         About
       </Menu.Item>
