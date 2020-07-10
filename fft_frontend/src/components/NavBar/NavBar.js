55a86244ae3b405e8b5c6115e17b28eeddb5aae0
Joonas Partanen <partanen.joonas@gmail.com>
2020-07-10 23:16:56 +0300

    Add color mode toggle switch

--- fft_frontend/src/components/NavBar/NavBar.js
+++ fft_frontend/src/components/NavBar/NavBar.js
@@ -1,7 +1,11 @@
 import React from 'react'
-import { Menu, Dropdown } from 'semantic-ui-react'
+import { Menu, Dropdown, Checkbox, Icon } from 'semantic-ui-react'
+import { useDispatch } from 'react-redux'
+import { changeColorMode } from '../../reducers/colorModeReducer'
 
-const NavBar = ({ year, setYear }) => {
+const NavBar = ({ year, setYear, darkModeActive }) => {
+  const dispatch = useDispatch()
+
   const years = [
     { key: 2019, text: '2019', value: 2019 },
     { key: 2018, text: '2018', value: 2018 },
@@ -10,11 +14,11 @@ const NavBar = ({ year, setYear }) => {
     { key: 2015, text: '2015', value: 2015 },
     { key: 2014, text: '2014', value: 2014 },
     { key: 2013, text: '2013', value: 2013 },
-    { key: 2012, text: '2012', value: 2012 }
+    { key: 2012, text: '2012', value: 2012 },
   ]
 
   return (
-    <Menu inverted stackable size='large' style={{ backgroundColor: '#333', marginBottom: '0px' }}>
+    <Menu as='nav' stackable size='large' className={darkModeActive ? 'navbar dark-mode' : 'navbar'}>
       <Menu.Item header>Finnish Foreign Trade Visualized</Menu.Item>
       <Menu.Item link as='a' href='#trade-map'>
         Trade Map
@@ -36,6 +40,11 @@ const NavBar = ({ year, setYear }) => {
           selection
           value={year}
         />
+      </Menu.Item>
+      <Menu.Item className="color-mode-toggle">
+        <Icon name='moon'></Icon>
+        <Checkbox onChange={() => dispatch(changeColorMode())} toggle></Checkbox>
+        <Icon name='sun'></Icon>
       </Menu.Item>
     </Menu>
   )
