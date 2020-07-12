6e483a44e0a8534aa42290bb2103fe2d30aeaa97
Joonas Partanen <partanen.joonas@gmail.com>
2020-07-12 22:57:27 +0300

    Simplify country codes state

--- fft_frontend/src/reducers/countryReducer.js
+++ fft_frontend/src/reducers/countryReducer.js
@@ -1,25 +1,22 @@
 import getCountryCodes from '../services/countryService'
 
-const countryReducer = (state = {}, action) => {
+const countryCodes = (state = [], action) => {
   switch (action.type) {
     case 'INIT_COUNTRYCODES':
-      return {
-        ...state,
-        countryCodes: action.countryCodes,
-      }
+      return action.payload
     default:
       return state
   }
 }
 
 export const initializeCountryCodes = () => {
-  return async (dispatch) => {
+  return async dispatch => {
     const countryCodes = await getCountryCodes()
     dispatch({
       type: 'INIT_COUNTRYCODES',
-      countryCodes: countryCodes,
+      payload: countryCodes,
     })
   }
 }
 
-export default countryReducer
+export default countryCodes
