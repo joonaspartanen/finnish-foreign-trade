729f1e6d5d0776ea78ff209951ec4c69b7abe799
Joonas Partanen <partanen.joonas@gmail.com>
2020-07-12 22:58:36 +0300

    Simplify reducer state management

--- fft_frontend/src/store.js
+++ fft_frontend/src/store.js
@@ -1,14 +1,14 @@
 import { applyMiddleware, combineReducers, createStore } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
-import countryReducer from './reducers/countryReducer'
-import tradeDataReducer from './reducers/tradeDataReducer'
-import colorModeReducer from './reducers/colorModeReducer'
+import countryCodes from './reducers/countryReducer'
+import tradeData from './reducers/tradeDataReducer'
+import darkModeActive from './reducers/colorModeReducer'
 
 const combinedReducer = combineReducers({
-  tradeData: tradeDataReducer,
-  countryData: countryReducer,
-  colorMode: colorModeReducer
+  tradeData,
+  countryCodes,
+  darkModeActive,
 })
 
 const store = createStore(combinedReducer, composeWithDevTools(applyMiddleware(thunk)))
